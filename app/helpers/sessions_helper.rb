312ca0059ba56bfcdcbaf381f94948f74c45a3b5
Keijitadokoro <gccx.kt.0925@icloud.com>
2020-11-10 16:06:50 +0000

    ヘッダー実装中

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,18 @@
 module SessionsHelper
+	#引数に渡されたuserオブジェクトでログインする。
+	def log_in(user)
+		session[:user_id] = user.id
+	end
+	
+	#現在ログイン中のユーザーがいる場合オブジェクトを返す「
+	def current_user
+		if session[:user_id]
+			@current_user ||= User.find_by(id: session[:user_id])
+		end
+	end
+	
+	#現在ログイン中のユーザーがいればtrue、そうでなければfalseを返す。
+	def logged_in?
+		!current_user.nil?
+	end
 end
