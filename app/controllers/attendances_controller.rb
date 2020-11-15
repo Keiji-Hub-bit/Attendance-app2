c670c99bfa5b8d3a558d9edf7eda17821680d627
Keijitadokoro <gccx.kt.0925@icloud.com>
2020-11-15 02:19:08 +0000

    勤怠B　10.6.2　出社退社実装完了

--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -5,11 +5,17 @@ class AttendancesController < ApplicationController
 	  @attendance = Attendance.find(params[:id])
 	  #出勤時間が未登録であることを判定
 	  if @attendance.started_at.nil?
-	  	if @attendance.update_attributes(started_at: Time.current.change(sec:o))
+	  	if @attendance.update_attributes(started_at: Time.current.change(sec:0))
 	  		flash[:info] = "おはようございます。"
 	  	else
-	  		flash[:danger] = "勤怠登録に失敗しました。やり直してください。"
+	  		flash[:danger] = UPDATE_ERROR_MSG
 	  	end
+	  elsif @attendance.finished_at.nil?
+      if @attendance.update_attributes(finished_at: Time.current.change(sec:0))
+	  		flash[:info] = "お疲れ様でした。"
+      else
+	    	flash[:danger] = UPDATE_ERROR_MSG
+      end
 	  end
 	  redirect_to @user
   end
