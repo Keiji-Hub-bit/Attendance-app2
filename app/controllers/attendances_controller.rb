a3f01eaea554485867bdc207a580b2d01a5e26a1
Keijitadokoro <gccx.kt.0925@icloud.com>
2020-11-17 11:25:34 +0000

    勤怠B 11.2ストロングパラメーターの応用

--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -2,6 +2,7 @@ class AttendancesController < ApplicationController
 	before_action :set_user, only: :edit_one_month
   before_action :logged_in_user, only: [:update, :edit_one_month]
   before_action :set_one_month, only: :edit_one_month
+
 	UPDATE_ERROR_MSG = "勤怠登録に失敗しました。やり直してください。"
 	
   def update
@@ -29,4 +30,10 @@ class AttendancesController < ApplicationController
   
   def update_one_month
   end
+  
+  private
+    #1ヶ月分の勤怠情報を扱います。
+    def attendances_params
+    	params.require(:user).permit(attendances:[:started_at, :finished_at, :note])[:attendances]
+    end	
 end
